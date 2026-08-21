d5dc1f033bb6c37d8fcd2a1b28c6890b4b9dd65c
Daniel Ceron <dceroncastillo@unimayor.edu.co>
2026-08-21 11:37:11 -0500

    feat (clase 3): introduccion a js ejercicio 1

---
+++ clase-3-intro-javascript/scripts/script.js
@@ -0,0 +1,11 @@
+let primerNombre = "Daniel"
+let edad = 20
+let direccion
+let esMayorDeEdad = true
+let cantidad = NaN
+let inventario = null
+let resultado = (3-2) * (10/2)
+
+console.log(resultado)
+console.log(resultado++)
+console.log(resultado)
