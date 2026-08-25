a9fcf5438750013d7d1b3b4b63111ab3e8a36aa8
juan garcia <juangarcia@unimayor.edu.co>
2026-08-25 11:22:57 -0500

    feat:acceso a mi elemento y modifico su color a verde

--- clase-4-javascript-2/juan-garcias/selectores-normales/script.js
+++ clase-4-javascript-2/juan-garcias/selectores-normales/script.js
@@ -1,3 +1,8 @@
 miElemento = document.getElementById("miElemento");
 miElemento.style.color = "#2735F5";
 miElemento.innerHTML = " Hola Mundo!!!"
+
+let elementos = document.getElementsByClassName("mi-clase");
+console.log(elementos);
+elementos[0].style.color = "green";
+elementos[0].innerHTML = "Hola mundo!"
