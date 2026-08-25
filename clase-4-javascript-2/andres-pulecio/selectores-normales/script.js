f4cb09bfcc0f2d6348ad1983c5c5d08f5d3a0db9
Andres Pulecio <jpulecio@unimayor.edu.co>
2026-08-25 11:14:55 -0500

    feat: cambie el color de mi elemento a azul

--- clase-4-javascript-2/andres-pulecio/selectores-normales/script.js
+++ clase-4-javascript-2/andres-pulecio/selectores-normales/script.js
@@ -1,3 +1,6 @@
 miElemento = document.getElementById("mi-elemento");
 
 miElemento.innerHTML = "HOLA MUNDO!!!";
+
+// Cambiar el estilo de un elemento con id="mi-elemento"
+miElemento.style.color = "blue"; // Cambia el color del texto a azul
