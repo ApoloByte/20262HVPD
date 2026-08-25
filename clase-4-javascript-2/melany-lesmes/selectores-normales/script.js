19f1689ffafdeae9fc7f31b18ea757042cc2877f
Victor Pinto Personal <victorp2304.pro@gmail.com>
2026-08-25 11:24:47 -0500

    feat (clase-4-agos): "Cambios con inner.HTML y el style color"

---
+++ clase-4-javascript-2/melany-lesmes/selectores-normales/script.js
@@ -0,0 +1,10 @@
+//Primer ejercicio
+let elemento= document.getElementById("mi-elemento");
+console.log(elemento);
+
+//Segundo ejercicio
+elemento.innerHTML= "Hola Mundooo!!!";
+console.log(elemento); 
+
+//Tercer ejercicio
+elemento.style.color="green"
