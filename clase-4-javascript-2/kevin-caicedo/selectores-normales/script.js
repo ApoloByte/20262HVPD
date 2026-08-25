a2c3a4c4d3fdcea8e8792ea12b415bf6d6c9f53d
Kevin Caicedo <k.caicedo@unimayor.edu.co>
2026-08-25 11:25:27 -0500

    feat(ejercicios) : cambio de color de mi-elemento

--- clase-4-javascript-2/kevin-caicedo/selectores-normales/script.js
+++ clase-4-javascript-2/kevin-caicedo/selectores-normales/script.js
@@ -1,3 +1,7 @@
 miElemento = document.getElementById("mi-elemento");
 
 miElemento.innerHTML = "HOLA, MUNDO!!"
+
+// Cambiar el estilo de un elemento con id="mi-elemento"
+let elemento = document.getElementById("mi-elemento");
+elemento.style.color = "blue"; 
