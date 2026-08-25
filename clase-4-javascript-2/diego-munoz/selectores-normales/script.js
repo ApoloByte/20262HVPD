ecc7fb08c42cc157ffe18cb3b84666ecf6a47c69
Diego Munoz <df.munoz@unimayor.edu.co>
2026-08-25 12:18:48 -0500

    feat(clase-5): 1 ejercicio selectores normales cambia de color del elemento a amarillo

--- clase-4-javascript-2/diego-munoz/selectores-normales/script.js
+++ clase-4-javascript-2/diego-munoz/selectores-normales/script.js
@@ -1,3 +1,10 @@
 miElemento = document.getElementById("mi-elemento");
 
-miElemento.innerHTML = "Hola mundo!!!"
+miElemento.innerHTML = "Hola mundo!"
+
+let elemento = document.getElementById("mi-elemento");
+elemento.style.color = "yellow";
+
+let elementos = document.getElementsByClassName("mi-clase");
+console.log(elementos);
+
