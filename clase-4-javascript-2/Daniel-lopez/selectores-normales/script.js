b4c7aaf18f865a064cd2687efb024c598e0af8ad
Daniel lopez 1 <daniellopez@unimayor.edu.co>
2026-08-25 11:54:59 -0500

    feat: final de punto C. Modificar elementos fondo amarillo y 1 px

--- clase-4-javascript-2/Daniel-lopez/selectores-normales/script.js
+++ clase-4-javascript-2/Daniel-lopez/selectores-normales/script.js
@@ -6,3 +6,13 @@ let elementos = document.getElementsByClassName('mi-clase');
 console.log(elementos);
 elementos[0].style.color = "green";
 elementos[0].innerHTML = "Hola mundo!"
+elementos[1].style.color = "green";
+elementos[1].innerHTML = "Hola mundo!"
+elementos[2].style.color = "green";
+elementos[2].innerHTML = "Hola mundo!"
+
+let parrafos = document.getElementsByTagName('p');
+console.log(parrafos)
+for (let i = 0; i <parrafos.length; i++) {
+	parrafos[i].innerHTML = 'Hola mundo! ' + (i + 1);
+}
