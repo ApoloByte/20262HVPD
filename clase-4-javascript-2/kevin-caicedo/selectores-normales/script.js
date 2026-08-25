0d47b8406c0aaa994561e7df549268fb0e7417e7
Kevin Caicedo <k.caicedo@unimayor.edu.co>
2026-08-25 12:04:18 -0500

    feat (clase 5 punto b.):  acceso a elementos por clase, mas cambio a de color a verde

--- clase-4-javascript-2/kevin-caicedo/selectores-normales/script.js
+++ clase-4-javascript-2/kevin-caicedo/selectores-normales/script.js
@@ -5,3 +5,10 @@ miElemento.innerHTML = "HOLA, MUNDO!!"
 // Cambiar el estilo de un elemento con id="mi-elemento"
 let elemento = document.getElementById("mi-elemento");
 elemento.style.color = "blue"; 
+
+// Modificar el contenido de elementos con class="mi-clase"
+let elementos = document.getElementsByClassName("mi-clase");
+for (var i = 0; i < elementos.length; i++) {
+	elementos[i].innerHTML = "hola, Mundo!";
+    elementos[i].style.color = "green";
+}
