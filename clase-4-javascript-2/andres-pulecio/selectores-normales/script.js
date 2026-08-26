9f581ce690ec8882b68dc90ec1d7e60516040aba
Andres Pulecio <jpulecio@unimayor.edu.co>
2026-08-25 23:48:04 -0500

    feat: hice los tres elementos y les cambie el contenido y color

--- clase-4-javascript-2/andres-pulecio/selectores-normales/script.js
+++ clase-4-javascript-2/andres-pulecio/selectores-normales/script.js
@@ -1,6 +1,32 @@
-miElemento = document.getElementById("mi-elemento");
+//actividad
+let miElemento = document.getElementById("mi-elemento");
+
+console.log(miElemento); // Muestra el elemento en la consola
 
 miElemento.innerHTML = "HOLA MUNDO!!!";
 
 // Cambiar el estilo de un elemento con id="mi-elemento"
 miElemento.style.color = "blue"; // Cambia el color del texto a azul
+//actividad 2
+
+
+let elementoCasa = document.getElementById("mi-clase");
+
+elementoCasa.innerHTML = "HOLA CORAZON DE MELON!!!";
+
+elementoCasa.style.color = "green"; // Cambia el color del texto a verde
+
+
+
+let miCasa = document.getElementById("mi-casa");
+
+miCasa.innerHTML = "Recuerdas aquella tarde";
+
+miCasa.style.color = "orange"; // Cambia el color del texto a naranja
+
+
+let miUniversidad = document.getElementById("mi-universidad");
+
+miUniversidad.innerHTML = "Sonreias tanto ";
+
+miUniversidad.style.color = "red"; // Cambia el color del texto a morado
