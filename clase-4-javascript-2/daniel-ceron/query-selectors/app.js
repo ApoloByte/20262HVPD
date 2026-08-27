aa53edd0bdf24b5afaaffbf34e99363214b276cd
Daniel Ceron <dceroncastillo@unimayor.edu.co>
2026-08-26 23:30:02 -0500

    feat: Agrega la clase "activo" de los enlaces con id "menu"

---
+++ clase-4-javascript-2/daniel-ceron/query-selectors/app.js
@@ -0,0 +1,13 @@
+let documento = document.getElementsByClassName("destacado")
+
+for (let i = 0; i < documento.length; i++) {
+    documento[i].style.backgroundColor = 'yellow';
+    documento[i].style.fontWeight = 'bold';
+}
+
+console.log(elementosA);
+
+let enlaces = document.querySelector("#menu a")
+enlaces.classList.add("activo")
+
+console.log(enlaces)
