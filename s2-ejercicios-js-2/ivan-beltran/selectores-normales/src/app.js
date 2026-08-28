30423c71b9567da66bbea612bbe1ea8a8c76007d
ivan beltran <ichimunja@unimayor.edu.co>
2026-08-27 19:14:54 -0500

    Segundo Commit (Selectores Normales):ivan-beltran

--- s2-ejercicios-js-2/ivan-beltran/selectores-normales/src/app.js
+++ s2-ejercicios-js-2/ivan-beltran/selectores-normales/src/app.js
@@ -2,3 +2,10 @@ const elemento = document.getElementById("mi-elemento");
 
 elemento.textContent = "Hola, mundo!";
 elemento.style.backgroundColor = "blue";
+
+const elementos = document.getElementsByClassName("mi-clase");
+
+for (const elemento of elementos) {
+	elemento.textContent = "Hola, mundo!";
+	elemento.style.color = "green";
+}
