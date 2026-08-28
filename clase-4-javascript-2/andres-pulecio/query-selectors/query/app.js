e7ccd821002080591751acb04be719c79acda052
Andres Pulecio <jpulecio@unimayor.edu.co>
2026-08-28 13:23:26 -0500

    feat(semana-3): js-2 2.i

--- clase-4-javascript-2/andres-pulecio/query-selectors/query/app.js
+++ clase-4-javascript-2/andres-pulecio/query-selectors/query/app.js
@@ -1,40 +1,39 @@
-     const ELEMENTOS = document.querySelectorAll('.destacado');
+const ELEMENTOS = document.querySelectorAll('.destacado');
 
-     ELEMENTOS.forEach(elemento => {
-      elemento.style.backgroundColor = 'yellow';
-      elemento.style.border = 'yellow';
-     });
+ELEMENTOS.forEach(elemento => {
+    elemento.style.backgroundColor = 'yellow';
+    elemento.style.border = 'yellow';
+});
 
-     const LINK_ELEMENTS = document.querySelectorAll('#menu a');
+const LINK_ELEMENTS = document.querySelectorAll('#menu a');
 
-     LINK_ELEMENTS.forEach(elemento => {
-       elemento.classList.add('activo')
-     });
+LINK_ELEMENTS.forEach(elemento => {
+    elemento.classList.add('activo')
+});
 
-     /*let enlaces = document.querySelectorAll('#menu a');
-     enlaces.forEach(function(elemento) {
-        elemento.classList.add('activo');
-     });*/
+/*let enlaces = document.querySelectorAll('#menu a');
+enlaces.forEach(function(elemento) {
+   elemento.classList.add('activo');
+});*/
 
-     const IMG_ELEMENTS = document.querySelectorAll('img[alt]');
-      IMG_ELEMENTS.forEach(elemento => {
-        elemento.style.border = "3px solid blue";
-     });
+const IMG_ELEMENTS = document.querySelectorAll('img[alt]');
+IMG_ELEMENTS.forEach(elemento => {
+    elemento.style.border = "3px solid blue";
+});
 
 
-     const LI_ELEMENTS = document.querySelectorAll('.tareas li:nth-child(odd)');
-     console.log('Encontrados:', LI_ELEMENTS.length);
-     LI_ELEMENTS.forEach(elemento =>{
-         elemento.style.fontWeight = "bold";
-         elemento.style.color = "red";
-     });
+const LI_ELEMENTS = document.querySelectorAll('.tareas li:nth-child(odd)');
+console.log('Encontrados:', LI_ELEMENTS.length);
+LI_ELEMENTS.forEach(elemento => {
+    elemento.style.fontWeight = "bold";
+    elemento.style.color = "red";
+});
 
-     function desmarcarCheckboxes() {
-    let marcados = document.querySelectorAll('input[type="checkbox"]:checked');
-    marcados.forEach(function(checkbox) {
-        checkbox.checked = false;
+function inputSelectorUncheck() {
+    const marcados = document.querySelectorAll('input[type="checkbox"]:checked');
+    marcados.forEach(elemento => {
+        elemento.checked = false;
     });
 }
 
 // Uso:
-   document.getElementById('ejercicio5').addEventListener('click', desmarcarCheckboxes);
