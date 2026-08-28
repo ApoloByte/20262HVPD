906bd37c334cee2b09b6ba6bd4176734ec75a615
juan garcia <juangarcia@unimayor.edu.co>
2026-08-28 12:49:54 -0500

    feat(semana 3):ejercicio.2.js 2.f

--- clase-4-javascript-2/juan-garcias/query-selectors/query/app.js
+++ clase-4-javascript-2/juan-garcias/query-selectors/query/app.js
@@ -1,3 +1,9 @@
-
-
-
+const ELEMENTOS = document.querySelectorAll('.destacado');
+ELEMENTOS.forEach(elemento => {
+    elemento.style.background = 'yellow';
+    elemento.style.border = '5px solid orange';
+});
+const LINKELEMENTS = document.querySelectorAll('#menu a');
+LINKELEMENTS.forEach(elemento => {
+    elemento.classList.add('activo');
+});
