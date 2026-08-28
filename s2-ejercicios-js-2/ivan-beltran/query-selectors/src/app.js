96f01d750ced38b2f38e4df05d7bde6f7570fd01
ivan beltran <ichimunja@unimayor.edu.co>
2026-08-27 19:37:33 -0500

    Segundo Commit (Query Selectors):ivan-beltran

--- s2-ejercicios-js-2/ivan-beltran/query-selectors/src/app.js
+++ s2-ejercicios-js-2/ivan-beltran/query-selectors/src/app.js
@@ -9,3 +9,47 @@ const enlacesDelMenu = document.querySelectorAll("#menu a");
 for (const enlace of enlacesDelMenu) {
     enlace.classList.add("activo");
 }
+
+const imagenesConAlt = document.querySelectorAll("img[alt]");
+
+for (const imagen of imagenesConAlt) {
+    imagen.style.border = "3px solid blue";
+}
+
+const tareasImpares = document.querySelectorAll(".tareas li:nth-child(odd)");
+
+for (const tarea of tareasImpares) {
+    tarea.style.fontWeight = "bold";
+}
+
+function desmarcarCheckboxMarcados() {
+    const checkboxMarcados = document.querySelectorAll(
+        'input[type="checkbox"]:checked'
+    );
+
+    for (const checkbox of checkboxMarcados) {
+        checkbox.checked = false;
+    }
+}
+
+document
+    .querySelector("#ejercicio5")
+    .addEventListener("click", desmarcarCheckboxMarcados);
+
+let numeroDeTarea = 0;
+
+document.querySelector("#agregar-tarea").addEventListener("click", () => {
+    const contenedor = document.querySelector("#lista-tareas");
+    let lista = contenedor.querySelector("ul");
+
+    if (!lista) {
+        lista = document.createElement("ul");
+        contenedor.appendChild(lista);
+    }
+
+    numeroDeTarea += 1;
+
+    const nuevaTarea = document.createElement("li");
+    nuevaTarea.textContent = `Tarea ${numeroDeTarea}`;
+    lista.appendChild(nuevaTarea);
+});
