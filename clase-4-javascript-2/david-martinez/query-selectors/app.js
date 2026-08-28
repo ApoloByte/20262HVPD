44e61f6344d5c9a693d37ab24fa00f31d7fedc7a
David Martinez <ds.martinez@unimayor.edu.co>
2026-08-28 13:03:32 -0500

    feat (semana-3): ejercicio 2js 2.g

--- clase-4-javascript-2/david-martinez/query-selectors/app.js
+++ clase-4-javascript-2/david-martinez/query-selectors/app.js
@@ -5,8 +5,16 @@ ELEMENTS.forEach(elemento => {
   elemento.style.border = '8px solid orange';
 });
 
-const LINK_ELEMENTS = document.querySelectorAll('#menu a');
+const LINK_ELEMENTS = document.querySelectorAll("#menu a");
 
 LINK_ELEMENTS.forEach(elemento => {
     elemento.classList.add('activo');
 }); 
+
+const IMG_ELEMENTS = document.querySelectorAll("img[alt]");
+
+IMG_ELEMENTS.forEach(elemento => {
+    elemento.style.border = "3px solid blue";
+});
+
+
