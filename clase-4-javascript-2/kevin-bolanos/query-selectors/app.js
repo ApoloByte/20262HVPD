b0738662f037e90cb9dc681408ac2b42da5cebc9
Kevin Bolanos 1 <kbolanos@unimayor.edu.co>
2026-08-28 13:18:28 -0500

    feat (semana-3): ejercicio 2js 2.g

--- clase-4-javascript-2/kevin-bolanos/query-selectors/app.js
+++ clase-4-javascript-2/kevin-bolanos/query-selectors/app.js
@@ -1,17 +1,27 @@
-
 const ELEMENTOS = document.querySelectorAll('.');
 
 ELEMENTOS.forEach(elemento =>{
     elemento.style.backgroundColor = "yellow";
     elemento.style.border = "8px solid #ffa263;";
-
 });
 
 const LINK_ELEMENTS = document.querySelectorAll("#menu a");
 
 LINK_ELEMENTS.forEach(elemento =>{
     elemento.classList.add = ('activo');
+});
 
+const IMG_ELEMENTS = document.querySelectorAll("img[alt]");
+
+IMG_ELEMENTS.forEach(elemento =>{
+    elemento.classList.add = "3px solid blue";
+});
+
+const LI_ELEMENTS = document.querySelectorAll('input[type="checkbox"]:checked');
+
+LI_ELEMENTS.forEach(elemento =>{
+    elemento.style.fontWeight = "bold";
+    elemento.style.color = "red";
 });
 
 
