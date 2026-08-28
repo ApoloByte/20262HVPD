30b592ea1251f9aa35dff95ba18b5b6a7114688f
melany lesmes <melany.lemes@unimayor.edu.co>
2026-08-27 20:55:10 -0500

    feat (clase-4): punto c - ejercicio i con 'p' de parrafos, parte ii cambio de contenido y parte iii cambio de fondo amarillo con borde

--- clase-4-javascript-2/melany-lesmes/selectores-normales/script.js
+++ clase-4-javascript-2/melany-lesmes/selectores-normales/script.js
@@ -7,17 +7,28 @@ elemento.innerHTML= "Hola Mundooo!!!";
 console.log(elemento); 
 
 //iii
-elemento.style.color="blue"
+elemento.style.backgroundColor = "blue";
 
 // --- ejercicio b (ii) ---
 let miClase= document.getElementsByClassName("mi-clase");
 for (var i=0; i < miClase.length; i++){
     miClase[i].innerHTML="Hola, mundo!"
 }
-
 // iii
 for (var i=0; i < miClase.length; i++){
     miClase[i].style.color="green";
 }
-
 console.log(miClase);
+//--- ejercicio c (ii) ---
+let parrafos= document.getElementsByTagName("p");
+for (let i=0; i < parrafos.length; i++){
+    parrafos[i].innerHTML="Hola mundo";
+}
+// iii
+for (let i=0; i < parrafos.length; i++){
+    parrafos[i].style.backgroundColor="yellow";
+    //Borde en el background
+    parrafos[i].style.border="1px solid black";
+}
+
+console.log(parrafos);
