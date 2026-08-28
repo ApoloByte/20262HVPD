08342763313d419afbf33e0d4e96f81edbce9ff7
juan garcia <juangarcia@unimayor.edu.co>
2026-08-28 13:23:45 -0500

    feat(semana 3)ejercicio 2.js 2g.h.i

--- clase-4-javascript-2/juan-garcias/query-selectors/query/app.js
+++ clase-4-javascript-2/juan-garcias/query-selectors/query/app.js
@@ -12,3 +12,15 @@ const IMGELEMENTS = document.querySelectorAll('img[alt]');
 IMGELEMENTS.forEach(elemento => {
     elemento.style.border = '3px solid red';
 });
+
+const LI_ELEMENTS = document.querySelectorAll('.tareas li:nth-child(odd)');
+LI_ELEMENTS.forEach(elemento => {
+    elemento.style.fontWeight = 'bold';
+    elemento.style.color = 'red';
+});
+function inputSelectorUncheck() {
+    const CHECKBOX_ELEMENTS = document.querySelectorAll('input[type="checkbox"]:checked');
+    CHECKBOX_ELEMENTS.forEach(elemento => {
+        elemento.checked = false;
+    });
+}
