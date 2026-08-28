f20dba168d822ee6a3452480c24dc0da5157f3b9
Carlos Bucheli <cabucheli@unimayor.edu.co>
2026-08-28 12:49:58 -0500

    feat (semana-3): ejercicio 2 js. 2.f

--- Clase-4-javascript-2/carlos-bucheli/query-selectors/query/app.js
+++ Clase-4-javascript-2/carlos-bucheli/query-selectors/query/app.js
@@ -1,2 +1,19 @@
-let primerDev = document.querySelector('#mi-elemento');
-console.log(primerDev)
+const ELEMENTOS = document.querySelectorAll('.destacado');
+
+ELEMENTOS.forEach(elemento =>{
+
+    elemento.style.backgroundColor = "yellow";
+    
+
+});
+
+
+const linkElement = document.querySelectorAll("#menu a");
+
+
+LINK_ELEMENTS.forEach(elemento =>{
+
+    elemento.classList.add('activo');
+
+
+})
