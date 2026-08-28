124d90c759481dc8801fb0e1697d940590a156d0
Victor Pinto 4 <victorpinto@unimayor.edu.co>
2026-08-28 12:49:35 -0500

    feat (semana-3): ejercicio 2 js 2.f

--- clase-4-javascript-2/victor-pinto/query-selectors/query/app.js
+++ clase-4-javascript-2/victor-pinto/query-selectors/query/app.js
@@ -4,3 +4,9 @@ ELEMENTOS.forEach(elemento => {
     elemento.style.backgroundColor = "yellow";
     elemento.style.border = "8px solid #ffa263;";
 });
+
+const LINK_ELEMENTS = document.querySelectorAll("#menu a");
+
+LINK_ELEMENTS.forEach(elemento => {
+    elemento.classList.add('activo');
+});
