e1a84d363d39e4489e0895480738361a023801cf
Jhon Florez <jjflorez@unimayor.edu.co>
2026-08-29 14:17:39 -0500

    Ejercicio DOM: acceso por clase

--- ejercicio-js-2/jhon-florez/app.js
+++ ejercicio-js-2/jhon-florez/app.js
@@ -2,3 +2,10 @@ const elemento = document.getElementById("mi-elemento");
 
 elemento.textContent = "Hola, mundo!";
 elemento.style.backgroundColor = "blue";
+
+const elementos = document.getElementsByClassName("mi-clase");
+
+for (let i = 0; i < elementos.length; i++) {
+    elementos[i].textContent = "Hola, mundo!";
+    elementos[i].style.color = "green";
+}
