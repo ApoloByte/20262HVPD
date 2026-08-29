8e4a726d0091f6f51d39c589572ff528c9dd31bf
Diego Munoz <df.munoz@unimayor.edu.co>
2026-08-29 12:43:29 -0500

    feat-fix(Punto a y b): Cambio y creacion de un item cambiando el fondo a color azul y punto b creacion y alteracion del elemento y cambio de color a verde

--- clase-4-javascript-2/diego-munoz/selectores-normales/script.js
+++ clase-4-javascript-2/diego-munoz/selectores-normales/script.js
@@ -1,10 +1,11 @@
-miElemento = document.getElementById("mi-elemento");
-
-miElemento.innerHTML = "Hola mundo!"
-
-let elemento = document.getElementById("mi-elemento");
-elemento.style.color = "yellow";
+let miElemento = document.getElementById("mi-elemento");
+miElemento.innerHTML = "Hola mundo!";
+miElemento.style.backgroundColor = "blue";
 
 let elementos = document.getElementsByClassName("mi-clase");
 console.log(elementos);
 
+for (let i = 0; i < elementos.length; i++) {
+  elementos[i].innerHTML = "Hola mundo!";
+  elementos[i].style.color = "green";
+}
