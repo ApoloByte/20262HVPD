3880ebf08bb6ad63a278fabb185b535783988566
Diego Munoz <df.munoz@unimayor.edu.co>
2026-08-29 13:46:43 -0500

    feat(Ejercicio 2.G): Bordear las imagenes que tengan el atributo alt y darle un borde azul

--- clase-4-javascript-2/diego-munoz/query-selectors/app.js
+++ clase-4-javascript-2/diego-munoz/query-selectors/app.js
@@ -1,12 +1,21 @@
 const destacados = document.querySelectorAll('.destacado');
 destacados.forEach((elemento) => {
-	elemento.style.backgroundColor = 'yellow';
+    elemento.style.backgroundColor = 'yellow';
 });
 const boton2 = document.getElementById('ejercicio2');
 
 boton2.addEventListener('click', () => {
-  const enlaces = document.querySelectorAll('#menu a');
-  enlaces.forEach((enlace) => {
-    enlace.classList.add('activo');
-  });
+    const enlaces = document.querySelectorAll('#menu a');
+    enlaces.forEach((enlace) => {
+        enlace.classList.add('activo');
+    });
+});
+
+const boton3 = document.getElementById('ejercicio3');
+boton3.addEventListener('click', () => {
+    const imagenes = document.querySelectorAll('img[alt]');
+    imagenes.forEach((imagen) => {
+        imagen.style.border = '3px solid blue';
+    });
+
 });
