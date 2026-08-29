b372843dea05fe2469f400356e0f047ee462c00c
Diego Munoz <df.munoz@unimayor.edu.co>
2026-08-29 13:06:25 -0500

    Feat(Ejercicio 1.C): Creacion de 3 elementos de texto cambiando su contenido y agregando un fondo de color amarillo con bordeado

--- clase-4-javascript-2/diego-munoz/selectores-normales/script.js
+++ clase-4-javascript-2/diego-munoz/selectores-normales/script.js
@@ -9,3 +9,10 @@ for (let i = 0; i < elementos.length; i++) {
   elementos[i].innerHTML = "Hola mundo!";
   elementos[i].style.color = "green";
 }
+let parrafos = document.getElementsByTagName("p");
+
+for (let i = 0; i < parrafos.length; i++) {
+  parrafos[i].innerHTML = "Hola mundo";
+  parrafos[i].style.backgroundColor = "yellow";
+  parrafos[i].style.border = "1px solid black";
+}
