a16ef557704bb45392e8d22fc554f21067d93d24
Andres Pulecio <jpulecio@unimayor.edu.co>
2026-08-29 22:16:38 -0500

    feat: añadi el contador, el contenedor y la lista para las tareas

---
+++ clase-4-javascript-2/andres-pulecio/query-selectors/query-2/script.js
@@ -0,0 +1,7 @@
+let contador = 0;
+document.getElementById('btnAgregar').addEventListener('click', function(){
+    contador++;
+
+    let contenedor = document.getElementById('lista-tareas');
+    let lista = contenedor.querySelector('ul');
+});
