f90c2b207fdbad7c5291f27e63e0c15e4eb99ccd
melany lesmes <melany.lemes@unimayor.edu.co>
2026-08-29 22:59:03 -0500

    feat (clase-4) semana 3: ejercicios con uso math para generar numeros aleatorios, potencias y raices cuadradas

--- clase-4-javascript-2/melany-lesmes/objeto-math/app.js
+++ clase-4-javascript-2/melany-lesmes/objeto-math/app.js
@@ -20,3 +20,52 @@ function generarNumeroAleatorioRango(min,max){
     const NUMERO_MIN_MAX = Math.floor(Math.random() * (max - min + 1)) + min;
     MOSTRAR_P.innerHTML = NUMERO_MIN_MAX;
 }
+
+function generarPotenciaUno(base, exponente){
+    const MOSTRAR_P = document.getElementById("potencia-1");
+    const POTENCIA = Math.pow(base, exponente);
+    MOSTRAR_P.innerHTML = "Valor de " + base + " elevado a " + exponente + ": " + POTENCIA
+                        + " | Round: " + Math.round(POTENCIA) + " | Floor: " + Math.floor(POTENCIA)
+                        + " |Ceil: " + Math.ceil(POTENCIA);
+}
+
+
+function generarPotenciaDos(base, exponente){
+    const MOSTRAR_P = document.getElementById("potencia-2");
+    const POTENCIA = Math.pow(base, exponente);
+    MOSTRAR_P.innerHTML = "Valor de " + base + " elevado a " + exponente + ": " + POTENCIA
+                        + " | Round: " + Math.round(POTENCIA) + " | Floor: " + Math.floor(POTENCIA)
+                        + " |Ceil: " + Math.ceil(POTENCIA);
+}
+
+function generarPotenciaTres(base, exponente){
+    const MOSTRAR_P = document.getElementById("potencia-3");
+    const POTENCIA = Math.pow(base, exponente);
+    MOSTRAR_P.innerHTML = "Valor de " + base + " elevado a " + exponente + ": " + POTENCIA
+                        + " | Round: " + Math.round(POTENCIA) + " | Floor: " + Math.floor(POTENCIA)
+                        + " |Ceil: " + Math.ceil(POTENCIA);
+}
+
+function generarRaizUno(numero) {
+    const MOSTRAR_P = document.getElementById("raiz-1");
+    const RAIZ = Math.sqrt(numero);
+
+    MOSTRAR_P.innerHTML = "Valor real:" +RAIZ + " | Round: " + Math.round(RAIZ) 
+                        + " | Floor: " + Math.floor(RAIZ) + " | Ceil: " + Math.ceil(RAIZ);
+}
+
+function generarRaizDos(numero) {
+    const MOSTRAR_P = document.getElementById("raiz-2");
+    const RAIZ = Math.sqrt(numero);
+
+    MOSTRAR_P.innerHTML = "Valor real:" +RAIZ + " | Round: " + Math.round(RAIZ) 
+    + " | Floor: " + Math.floor(RAIZ) + " | Ceil: " + Math.ceil(RAIZ);
+}
+
+function generarRaizTres(numero) {
+    const MOSTRAR_P = document.getElementById("raiz-3");
+    const RAIZ = Math.sqrt(numero);
+
+    MOSTRAR_P.innerHTML = "Valor real:" +RAIZ + " | Round: " + Math.round(RAIZ) 
+    + " | Floor: " + Math.floor(RAIZ) + " | Ceil: " + Math.ceil(RAIZ);
+}
