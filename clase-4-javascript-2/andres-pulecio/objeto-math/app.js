c89dc1a11d60bb9e804e5db7ed2fe9ecb2957e94
Andres Pulecio <jpulecio@unimayor.edu.co>
2026-08-29 23:31:48 -0500

    feat: utilice la clase Math para crear numeros aleatorios en distintas condiciones

---
+++ clase-4-javascript-2/andres-pulecio/objeto-math/app.js
@@ -0,0 +1,21 @@
+//Primera Parte
+
+//Ejercicio1
+let primerRandom = Math.random() * (10 - 0) + 0;
+
+console.log(primerRandom);
+
+//Ejercicio2
+let segundoRandom = Math.random() * (10 - 0) + 0;
+
+console.log(segundoRandom);
+
+let segundoRandomEntero = Math.round(segundoRandom);
+
+console.log(segundoRandomEntero);
+
+//Ejercicio3 - numeros aleatorios entre 11 y 110
+
+let tercerRandom = Math.random() * (110 - 11) + 11;
+
+console.log(tercerRandom);
