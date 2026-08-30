0976d74c3c0949fa4df03aed95584259bdb5f360
Andres Pulecio <jpulecio@unimayor.edu.co>
2026-08-29 23:58:29 -0500

    refactor: Coreccion js-2 Math.3

--- clase-4-javascript-2/andres-pulecio/objeto-math/app.js
+++ clase-4-javascript-2/andres-pulecio/objeto-math/app.js
@@ -3,19 +3,39 @@
 //Ejercicio1
 let primerRandom = Math.random() * (10 - 0) + 0;
 
+console.log("Random entre 0 y 10");
+
 console.log(primerRandom);
+
+console.log("----------------------------");
 
 //Ejercicio2
 let segundoRandom = Math.random() * (10 - 0) + 0;
 
-console.log(segundoRandom);
+console.log("Random entero entre 0 y 10");
 
 let segundoRandomEntero = Math.round(segundoRandom);
 
 console.log(segundoRandomEntero);
 
+console.log("----------------------------");
+
 //Ejercicio3 - numeros aleatorios entre 11 y 110
 
 let tercerRandom = Math.random() * (110 - 11) + 11;
 
-console.log(tercerRandom);
+let tercerRandomEntero = Math.round(tercerRandom);
+
+console.log("Random entero entre 11 y 110");
+
+console.log(tercerRandomEntero);
+
+console.log("----------------------------");
+
+//Segunda Parte
+
+//Ejercicio1
+
+
+
+
--- clase-4-javascript-2/andres-pulecio/query-selectors/query/app.js
+++ clase-4-javascript-2/andres-pulecio/query-selectors/query/app.js
@@ -35,5 +35,3 @@ function inputSelectorUncheck() {
         elemento.checked = false;
     });
 }
-
-// Uso:
