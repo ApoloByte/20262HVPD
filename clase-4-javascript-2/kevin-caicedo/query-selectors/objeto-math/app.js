e161f226602330a6e978732a85915661dcc22e1b
Kevin Caicedo <k.caicedo@unimayor.edu.co>
2026-08-31 17:13:11 -0500

    feat(ejercicio):  objeto math, js-1,2,3 ultimos

--- clase-4-javascript-2/kevin-caicedo/query-selectors/objeto-math/app.js
+++ clase-4-javascript-2/kevin-caicedo/query-selectors/objeto-math/app.js
@@ -8,6 +8,22 @@ function getRandomArbitrary(min, max){
     return Math.random() * (max - min)+ min;
 }
 
-let numero = getRandomArbitrary(1, 10);
-console.log(numero);
+let numeros = getRandomArbitrary(1, 10);
+console.log(numeros);
 
+
+let numero = 3.6;
+
+console.log(numero);
+console.log(Math.round(numero));
+console.log(Math.ceil(numero));
+console.log(Math.floor(numero));
+
+
+console.log(Math.pow(4, 3));
+console.log(Math.pow(5, 2));
+console.log(Math.pow(5, -2));
+
+console.log(Math.sqrt(9));
+console.log(Math.sqrt(64));
+console.log(Math.sqrt(25));
