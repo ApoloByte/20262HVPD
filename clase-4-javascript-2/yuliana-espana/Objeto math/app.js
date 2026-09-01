443ff25cab545b6cae5d6f050ce6470ee641e675
Yuliana España <y.espana@unimayor.edu.co>
2026-09-01 15:49:09 -0500

    feat(clase-4): ej4 Objeto math  -  generar números aleatorios

---
+++ clase-4-javascript-2/yuliana-espana/Objeto math/app.js
@@ -0,0 +1,24 @@
+console.log("Primera Parte");
+console.log("Ejercicio 1");
+
+let primerRandom = Math.random() * (10 - 0) + 0;
+
+console.log("Random entre 0 y 10");
+
+console.log(primerRandom);
+
+console.log("Ejercicio 2");
+
+let segundoRandom = Math.round(primerRandom);
+
+console.log("Random redondeado");
+
+console.log(segundoRandom);
+
+let tercerRandom = Math.random() * (110 - 11) + 11;
+
+let tercerRandomEntero = Math.round(tercerRandom);
+
+console.log("Entero entre 11 y 110");
+
+console.log(tercerRandomEntero);
