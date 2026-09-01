7392c482a10ab8ef1594d8f7106ce8df3d470883
Yuliana España <y.espana@unimayor.edu.co>
2026-09-01 15:10:10 -0500

    feat(clase-4):ej3.B - ul dentro del contenedor

--- clase-4-javascript-2/yuliana-espana/query-selectors/query-2/app.js
+++ clase-4-javascript-2/yuliana-espana/query-selectors/query-2/app.js
@@ -8,4 +8,8 @@ boton.addEventListener('click', function () {
     const texto = document.createTextNode('Tarea ' + contador); 
     item.appendChild(texto); 
 
- 
+ let lista = div.querySelector('ul');
+    if (!lista) {
+        lista = document.createElement('ul');
+        div.appendChild(lista);
+    }
