933e1980666aed279db75b210a683e91647f4132
Daniel Ceron <dceroncastillo@unimayor.edu.co>
2026-09-01 17:22:34 -0500

    feat: Inicio de ejercicios del primer archivo js Punto 6

--- clase-3-intro-javascript/daniel-ceron/script/scripts/script.js
+++ clase-3-intro-javascript/daniel-ceron/script/scripts/script.js
@@ -5,7 +5,19 @@ let esMayorDeEdad = true
 let cantidad = NaN
 let inventario = null
 let resultado = (3-2) * (10/2)
-
-console.log(resultado)
-console.log(resultado++)
-console.log(resultado)
+console.log("===Ejercicios 1-3===")
+console.log(primerNombre)
+console.log(edad)
+console.log(direccion)
+console.log(esMayorDeEdad)
+console.log(cantidad)
+console.log(inventario)
+console.log("=========")
+console.log("===Ejercicio 5===")
+let resultado1 = (3 - 2) * (10 / 2); // Resultado: 5
+let resultado2 = 3 - 2 * 10 / 2; // Resultado: -7
+let resultado3 = (3 - 2) * 10 / 2; // Resultado: 5
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log("=========")
