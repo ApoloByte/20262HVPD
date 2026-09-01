fc6e9dc7079294927e651a87277c5b9fd37f6450
Andres Pulecio <jpulecio@unimayor.edu.co>
2026-08-31 22:10:22 -0500

    feat: le añadi mi toquer al ejercicio query-2

--- clase-4-javascript-2/andres-pulecio/query-selectors/query-2/script.js
+++ clase-4-javascript-2/andres-pulecio/query-selectors/query-2/script.js
@@ -7,7 +7,7 @@ document.getElementById('btnAgregar').addEventListener('click', function(){
 
     //1. crear el nuevo <li> con el texto "Tarea X"
     let nuevoItem = document.createElement('li');
-    nuevoItem.textContent = "Tarea" + contador;
+    nuevoItem.textContent = "Tarea " + contador;
     //2. Si aun no existe el <ul>, hay q crearlo
     if(!lista){
         lista = document.createElement('ul');
@@ -15,4 +15,24 @@ document.getElementById('btnAgregar').addEventListener('click', function(){
     } 
     //3. Lo anadimos al <ul> con appendChild
     lista.appendChild(nuevoItem);
+});
+
+let contadorChevecha = 0;
+
+document.getElementById('btnAgregarChevecha').addEventListener('click', function(){
+    contadorChevecha++;
+
+    let contenedorChevecha = document.getElementById('lista-polas');
+    let listaChevecha = contenedorChevecha.querySelector('ul');
+
+    //1. crear el nuevo <li> con el texto "Chevecha X"
+    let nuevoItem = document.createElement('li');
+    nuevoItem.textContent = "Chevecha " + contadorChevecha;
+    //2. Si aun no existe el <ul>, hay q crearlo
+    if(!listaChevecha){
+        listaChevecha = document.createElement('ul');
+        contenedorChevecha.appendChild(listaChevecha);
+    } 
+    //3. Lo anadimos al <ul> con appendChild
+    listaChevecha.appendChild(nuevoItem);
 });
