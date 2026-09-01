5808f1948d98dcc4123f271c0c37efb7687b18dc
melany lesmes <melany.lemes@unimayor.edu.co>
2026-08-31 20:23:15 -0500

    feat (clase-3): Metodos de busqueda reemplazo y casing ejercicios :D

--- clase-3-javascript/script/script.js
+++ clase-3-javascript/script/script.js
@@ -12,9 +12,19 @@ resultado3++;
 console.log(resultado3);
 
 
-let variable = "El desarrollo web es lo máximo";
+let variable = "El desarrollo web es lo maximo";
 
 //Metodos busqueda
 console.log("IndexOf(s): "+variable.indexOf("s"));
 console.log("LastIndexOf(x): "+variable.lastIndexOf("x"));
 console.log("Include(TDS): "+variable.includes("TDS"));
+
+//Metodos de reemplazo
+console.log("Texto original: " + variable +
+            " | Teexto replace: " + variable.replace("maximo", "DURO :D")
+);
+console.log ("ReplaceALL(e,E) "+ variable.replaceAll("e","E"));
+
+//Casing
+console.log("toUpperCase(): " +variable.toUpperCase());
+console.log("toLowerCase(): " + variable.toLowerCase());
