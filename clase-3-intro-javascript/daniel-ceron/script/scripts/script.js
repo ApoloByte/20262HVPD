f2d5b72e9535adf3bf99b7ae3b222dbdf410da0c
Daniel Ceron <dceroncastillo@unimayor.edu.co>
2026-09-01 11:49:33 -0500

    feat: pull origin dev a la rama del profesor

--- clase-3-intro-javascript/daniel-ceron/script/scripts/script.js
+++ clase-3-intro-javascript/daniel-ceron/script/scripts/script.js
@@ -1,5 +1,11 @@
-let resultado1 = (3-2) * (10/2);
+let primerNombre = "Daniel"
+let edad = 20
+let direccion
+let esMayorDeEdad = true
+let cantidad = NaN
+let inventario = null
+let resultado = (3-2) * (10/2)
 
-console.log(resultado1);
-console.log(resultado1++);
-console.log(resultado1);
+console.log(resultado)
+console.log(resultado++)
+console.log(resultado)
