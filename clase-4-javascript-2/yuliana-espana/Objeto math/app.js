60657cde07ae8d5a693c8ab19bbb74522aca4a96
Yuliana España <y.espana@unimayor.edu.co>
2026-09-01 15:56:55 -0500

    feat(clase-4): ej4 Objeto math  -  calculo de potencias y raíces cuadradas

--- clase-4-javascript-2/yuliana-espana/Objeto math/app.js
+++ clase-4-javascript-2/yuliana-espana/Objeto math/app.js
@@ -22,3 +22,47 @@ let tercerRandomEntero = Math.round(tercerRandom);
 console.log("Entero entre 11 y 110");
 
 console.log(tercerRandomEntero);
+
+
+console.log("Segunda Parte");
+
+let cuartoRandom = Math.random() * (999 - 0) + 0;
+
+console.log("Numero random");
+
+console.log(cuartoRandom);
+
+console.log("Numero con redondeo round");
+
+let cuartoRandomRound = Math.round(cuartoRandom);
+
+console.log(cuartoRandomRound);
+
+console.log("Numero con redondeo Ceil");
+let cuartoRandomCeil = Math.ceil(cuartoRandom);
+console.log(cuartoRandomCeil);
+
+console.log("Numero con redondeo Floor");
+let cuartoRandomFloor = Math.floor(cuartoRandom);
+console.log(cuartoRandomFloor);
+
+console.log("Segunda Parte");
+console.log("Ejercicio 2.a: 4^3");
+console.log("Resultado: " + Math.pow(4, 3));
+
+console.log("Ejercicio 2.b: 5^2");
+console.log("Resultado: " + Math.pow(5, 2));
+
+console.log("Ejercicio 2.c: 5^-2");
+console.log("Resultado: " + Math.pow(5, -2));
+
+console.log("Tercera Parte");
+console.log("Ejercicio 3.a: Raiz Cuadrada de 9");
+
+console.log("Resultado: " + Math.sqrt(9));
+console.log("Ejercicio 3.b: Raiz Cuadrada de 64");
+console.log("Resultado: " + Math.sqrt(64));
+
+console.log("Ejercicio 3.b: Raiz Cuadrada de 25");
+console.log("Resultado: " + Math.sqrt(25));
+
