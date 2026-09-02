4955455065e49ba3c7d3c5251c05cff3d71e8a91
Liney Anacona <l.anacona@unimayor.edu.coalias>
2026-09-01 23:30:41 -0500

    feat(actividad): Generar potencias

--- clase-4-javascript-2/liney-anacona/query-selectors/objeto-math/app.js
+++ clase-4-javascript-2/liney-anacona/query-selectors/objeto-math/app.js
@@ -16,3 +16,31 @@ function generarNumeroAleatorioRango(min,max){
     const numero_min_max = Math.floor(Math.random() * (max - min + 1)) + min;
     parrafo_resultado.innerHTML = numero_min_max;
 }
+
+function generarPotenciaUno(base, exponente){
+    const parrafo_resultado = document.getElementById("potenciaUno");
+    const potencia = Math.pow(base, exponente);
+    parrafo_resultado.innerHTML = base + " elevado a " + exponente + ": " + potencia+ "<br>" +
+        "Round: " + Math.round(potencia) + "<br>" +
+        "Floor: " + Math.floor(potencia) + "<br>" +
+        "Ceil: " + Math.ceil(potencia);
+}
+
+
+function generarPotenciaDos(base, exponente){
+    const parrafo_resultado = document.getElementById("potenciaDos");
+    const potencia = Math.pow(base, exponente);
+    parrafo_resultado.innerHTML = base + " elevado a " + exponente + ": " + potencia+ "<br>" +
+        "Round: " + Math.round(potencia) + "<br>" +
+        "Floor: " + Math.floor(potencia) + "<br>" +
+        "Ceil: " + Math.ceil(potencia);
+}
+
+function generarPotenciaTres(base, exponente){
+    const parrafo_resultado = document.getElementById("potenciaTres");
+    const potencia = Math.pow(base, exponente);
+    parrafo_resultado.innerHTML = base + " elevado a " + exponente + ": " + potencia+ "<br>" +
+        "Round: " + Math.round(potencia) + "<br>" +
+        "Floor: " + Math.floor(potencia) + "<br>" +
+        "Ceil: " + Math.ceil(potencia);
+}
