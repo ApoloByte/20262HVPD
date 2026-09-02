b0995f23b782c37916df7427a03f60a9e9b46738
Liney Anacona <l.anacona@unimayor.edu.coalias>
2026-09-01 20:24:24 -0500

    feat (actividad): desarrollo punto 1 subpunto c. acceso a elementos por etiqueta  y ensayo de acceso por clase y en linea, modificando su contenido cambiando bordesy colores

--- clase-4-javascript-2/liney-anacona/selectores-normales/script.js
+++ clase-4-javascript-2/liney-anacona/selectores-normales/script.js
@@ -1,33 +1,71 @@
+//-----------------ID--------------------------------
+
 miElemento = document.getElementById("mi-elemento");
 miElemento.innerHTML = "HOLA MUNDO!!!"
 miElemento.style.color = "blue"
 
-// Acceder a elementos con class="mi-clase"
+// ---------------CLASE---------------------------------
+
 let elementos = document.getElementsByClassName("mi-clase");
 console.log(elementos); // Muestra la colección de elementos en la consola
 
 // Modificar el contenido de elementos con class="mi-clase"
-let elemento = document.getElementsByClassName("mi-clase");
-for (var i = 0; i < elemento.length; i++) {
-	elemento[i].innerHTML = "Hola mundo!!";
+elementos = document.getElementsByClassName("mi-clase");
+for (var i = 0; i < elementos.length; i++) {
+	elementos[i].innerHTML = "Hola mundo!!";
 }
 
 // Cambiar el estilo de elementos con class="mi-clase"
-var element = document.getElementsByClassName("mi-clase");
-for (var i = 0; i < element.length; i++) {
-	element[i].style.color = "green"; // Cambia el color del texto a verde
+elementos = document.getElementsByClassName("mi-clase");
+for (var i = 0; i < elementos.length; i++) {
+	elementos[i].style.color = "green"; // Cambia el color del texto a verde
 }
 
 //OTRAS FRASES CON OTROS COLORES, ENSAYANDO 
-let ensayos = document.getElementsByClassName("my-clase");
-console.log(ensayos);
-
 let ensayo = document.getElementsByClassName("my-clase");
+console.log(ensayo);
+
+ensayo = document.getElementsByClassName("my-clase");
 for (var i = 0; i < ensayo.length; i++) {
 	ensayo[i].innerHTML = "Liney Anacona";
 }
 
-var ensay = document.getElementsByClassName("my-clase");
-for (var i = 0; i < ensay.length; i++) {
-	ensay[i].style.color = "pink"; 
+ensayo = document.getElementsByClassName("my-clase");
+for (var i = 0; i < ensayo.length; i++) {
+	ensayo[i].style.color = "pink"; 
+}
+
+//------------------ PARRAFOS------------------------- 
+
+let parrafos = document.getElementsByTagName("p");
+//parafos 
+for (let i = 0; i < parrafos.length; i++) {
+    parrafos[i].innerHTML = "Hola mundo";
+    parrafos[i].style.backgroundColor = "yellow";
+    parrafos[i].style.border = "1px solid";
+}
+
+//por clase 
+
+let amarillos = document.getElementsByClassName("amarillo");
+let rosados = document.getElementsByClassName("rosado");
+
+for (let i = 0; i < amarillos.length; i++) {
+	amarillos[i].innerHTML = "Experimentando en java script";
+    amarillos[i].style.backgroundColor = "gray";
+}
+
+for (let i = 0; i < rosados.length; i++) {
+	rosados[i].innerHTML = "Todo salio bien !!";
+    rosados[i].style.backgroundColor = "#90ee90";
+	rosados[i].style.border = "8px solid";
+}
+
+//en linea
+let segundoParrafo = document.getElementsByTagName("a");
+
+for (let i = 0; i < segundoParrafo.length; i++) {
+    segundoParrafo[i].innerHTML = "Colegio mayor";
+    segundoParrafo[i].style.backgroundColor = "pink";
+    segundoParrafo[i].style.border = "2px solid";
 }
