24c3d65258013017973891977823b155b36b4d59
juan garcia <juangarcia@unimayor.edu.co>
2026-09-02 23:24:55 -0500

    feat(clase 3)metodo de busqueda remplazo y casing

--- clase-3-intro-javascript/juan-garcia/script.js
+++ clase-3-intro-javascript/juan-garcia/script.js
@@ -13,3 +13,13 @@ let resultado3 = (3 - 2) * 10 / 2;
 console.log(`resultado3 = ${resultado3}`);
 console.log(`resultado3++ = ${++resultado3}`);
 console.log(`resultado3-- = ${resultado3}`);
+
+let texto = "El desarrollo web es lo maximo";
+
+console.log(texto.indexOf("es")); 
+console.log(texto.lastIndexOf("a")); 
+console.log(texto.includes("genial"));
+console.log(texto.replace("web", "movil"));
+console.log(texto.replaceAll("o", "O")); 
+console.log(texto.toUpperCase());
+console.log(texto.toLowerCase());
