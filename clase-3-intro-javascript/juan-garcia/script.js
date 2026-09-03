f0ff40b855e64c908d690ebee22737c79505e4d5
juan garcia <juangarcia@unimayor.edu.co>
2026-09-02 23:15:16 -0500

    feat(clase 3)utilizando operadores de incremento y decremento

--- clase-3-intro-javascript/juan-garcia/script.js
+++ clase-3-intro-javascript/juan-garcia/script.js
@@ -1,10 +1,15 @@
 let resultado1 = (3-2) * (10/2);
-console.log(`resultado1 = ${resultado1}`);
 
+console.log(`resultado1 = ${resultado1}`);
+console.log(`resultado1++ = ${++resultado1}`);
+console.log(`resultado1-- = ${resultado1}`);
 
 let resultado2 = 3 - 2 * 10 / 2;
 console.log(`resultado2 = ${resultado2}`);
-
+console.log(`resultado2++ = ${++resultado2}`);
+console.log(`resultado2-- = ${resultado2}`);
 
 let resultado3 = (3 - 2) * 10 / 2;
 console.log(`resultado3 = ${resultado3}`);
+console.log(`resultado3++ = ${++resultado3}`);
+console.log(`resultado3-- = ${resultado3}`);
