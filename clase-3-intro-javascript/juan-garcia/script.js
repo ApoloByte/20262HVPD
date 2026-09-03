69df1171ad639db22d320f7beff7e3a7b29f0bf4
juan garcia <juangarcia@unimayor.edu.co>
2026-09-02 23:11:53 -0500

    feat(clase 3)actividad js

--- clase-3-intro-javascript/juan-garcia/script.js
+++ clase-3-intro-javascript/juan-garcia/script.js
@@ -1,6 +1,10 @@
 let resultado1 = (3-2) * (10/2);
+console.log(`resultado1 = ${resultado1}`);
 
-console.log(resultado1);
-console.log(resultado1++);
-console.log(resultado1);
 
+let resultado2 = 3 - 2 * 10 / 2;
+console.log(`resultado2 = ${resultado2}`);
+
+
+let resultado3 = (3 - 2) * 10 / 2;
+console.log(`resultado3 = ${resultado3}`);
