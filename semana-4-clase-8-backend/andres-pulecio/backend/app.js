7e6718fb838a235c91f550e77204bccbefc2ffae
jpulecio-design <jpulecio@unimayor.edu.co>
2026-09-04 13:13:43 -0500

    fix correccion de ejercicio

--- semana-4-clase-8-backend/andres-pulecio/backend/app.js
+++ semana-4-clase-8-backend/andres-pulecio/backend/app.js
@@ -4,28 +4,24 @@ const app = express();
 
 const users = [
     {
-        name: 'Maria',
+        name: "María",
         age: 34,
-        email: "maria@gmail.com"
-
-    
+        email: "maria.gmail.com"
     },
     {
-        name: 'Fercho',
-        age: 67,
-        email: "fercho@gmail.com"
-
-    }
-
-]
+        name: "María",
+        age: 34,
+        email: "maria.gmail.com"
+    },
+];
 
 app.use(cors());
 
 app.get('/users', (req, res) => {
-	res.json(users); // envía la lista al cliente
+    res.json(users); // envía la lista al cliente
 });
 // TODO: CREAR AQUÍ LOS DEMÁS MÉTODOS
 app.listen(3000, () => {
-	console.log('El servidor está escuchando en el puerto 3000');
+    console.log('El servidor está escuchando en el puerto 3000');
 });
 
