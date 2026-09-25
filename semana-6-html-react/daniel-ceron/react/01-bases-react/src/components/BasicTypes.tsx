d89c50455748f8c1e8d4cb2631dbc6a2ae87780f
Daniel Ceron <dceroncastillo@unimayor.edu.co>
2026-09-25 13:19:44 -0500

    feat(semana-6):  Uso de variables dentro de mis componentes * Declaracion de Constantes con Definicion de Tipos

--- semana-6-html-react/daniel-ceron/react/01-bases-react/src/components/BasicTypes.tsx
+++ semana-6-html-react/daniel-ceron/react/01-bases-react/src/components/BasicTypes.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export const BasicTypes = () => {
+  const name: string = 'Víctor';
+  const age: number = 28;
+  const isActive: boolean = true;
+
   return (
-    <div>BasicTypes</div>
-  )
-}
+    <div>
+      <h2>BasicTypes</h2>
+      <p>Nombre: {name}</p>
+      <p>Edad: {age}</p>
+      <p>Activo: {isActive ? 'Sí' : 'No'}</p>
+    </div>
+  );
+};
