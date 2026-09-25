1fe49ea91d6b77eb3ddfc941e9e1b2491ff00693
melany lesmes <melany.lemes@unimayor.edu.co>
2026-09-25 13:06:37 -0500

    feat (sema-6): Ejercicio

--- semana-6-html-react/melany-lesmes/html/script.js
+++ semana-6-html-react/melany-lesmes/html/script.js
@@ -5,3 +5,19 @@ function nombreFuncion (){
 const funcion2 = ()=>{
     console.log("second")
 }
+const users =[
+    {
+        "name": "eduardo",
+        "age" : 30
+    },
+    {
+        "name": "karen",
+        "age" : 30
+    }
+]
+
+const funcion3 =()=>{
+    //callback
+    users.map ((user) => {console.log (user.name)})
+
+}
