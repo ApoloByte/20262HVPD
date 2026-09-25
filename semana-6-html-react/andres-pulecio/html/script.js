0093191875c70a849dce53abc1a0a763b368f013
jpulecio-design <jpulecio@unimayor.edu.co>
2026-09-25 16:48:30 -0500

    feat(semana 6-andres-pulecio): paquetes de react

--- semana-6-html-react/andres-pulecio/html/script.js
+++ semana-6-html-react/andres-pulecio/html/script.js
@@ -5,3 +5,15 @@ function nombreFuncion(){
 const funcion2 = ()=>{
     console.log("second")
 }
+const users = [
+    {
+        "name":"eduardo",
+        "age":30,
+    },
+    {
+        "name":"marla",
+        "age":33,
+    },
+]
+
+
