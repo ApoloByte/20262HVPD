ade1fa66ad8bb9209d86eab65066e916e8d8c5c8
Victor Pinto <victorpinto@unimayor.edu.co>
2026-09-25 13:19:34 -0500

    feat(semana-6): uso de constantes dentro de mis componentes
    * declaración de constantes con definición de tipos

--- semana-6-html-react/victor-pinto/react/01-bases-react/src/components/BasicTypes.tsx
+++ semana-6-html-react/victor-pinto/react/01-bases-react/src/components/BasicTypes.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 export const BasicTypes = () => {
-  return (
-    <div>BasicTypes</div>
-  )
+    const name: string = "Víctor";
+    const age: number = 28;
+    const isActive: boolean = true;
+    return (
+        <>
+            <h3>Basic Types</h3>
+            <ul>
+                <li>Nombre: {name}</li>
+                <li>Edad: {age}</li>
+            </ul>
+        </>
+    )
 }
