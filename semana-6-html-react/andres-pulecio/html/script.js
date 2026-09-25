03454491a5266af7dbf3a41c5d0f3efde6df8980
jpulecio-design <jpulecio@unimayor.edu.co>
2026-09-25 17:23:42 -0500

    feat(semana 6-andres-pulecio): hice los ejemplos de clase de como funciona una funcionen el script y html

--- semana-6-html-react/andres-pulecio/html/script.js
+++ semana-6-html-react/andres-pulecio/html/script.js
@@ -1,19 +1,26 @@
-function nombreFuncion(){
+function nombreFuncion() {
     console.log("first")
 }
 
-const funcion2 = ()=>{
+const funcion2 = () => {
     console.log("second")
 }
+
 const users = [
     {
-        "name":"eduardo",
-        "age":30,
+        "name": "eduardo",
+        "age": 30
     },
     {
-        "name":"marla",
-        "age":33,
+        "name": "karen",
+        "age": 30
     },
 ]
+const funcion3 = () => {
+    // callback
+    users.map((user) => { console.log(user["name"]) })
+    users.map((user) => console.log(user["name"]))
+    users.map((user) => { console.log(user.name) })
+}
 
 
